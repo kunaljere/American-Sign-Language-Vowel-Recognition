b099db863aa2ccd14c37d58c98572ff2386558df
kunaljere <43733660+kunaljere@users.noreply.github.com>
2022-10-25 00:20:56 -0400

    Add files via upload

---
+++ live_asl_recognition.py
@@ -0,0 +1,111 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import load_model
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import time
+import mediapipe as mp
+
+# letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['A', 'E', 'I', 'O', 'U']
+letters_with_labels = {letter : i for i, letter in enumerate(letters)}
+# print(letters_with_labels)
+
+# Defining image sizes
+IMG_WIDTH=200
+IMG_HEIGHT=200
+
+# Choose which model you would like to load
+# model = load_model('trained_model')
+# model = load_model('edge_detection_model')
+# model = load_model('binary_threshold_model')
+# model = load_model('grayscale_model')
+model = load_model('vowel_thresholding_model')
+
+# Setting width and height of webcam
+widthCam, heightCam = 640, 480
+
+# create VideoCapture object with camera as input (0)
+cap = cv2.VideoCapture(0)
+cap.set(3, widthCam)
+cap.set(4, heightCam)
+
+#initializing previous time
+prevTime = 0
+curTime = 0
+
+# Creating media pipe object
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
+       
+while(True):
+    # grabbing each frame of webcam footage
+    ret,img=cap.read() 
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+    '''MediaPipe Code'''
+    # Drawing lines and circles to track hand
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    '''Threshold Image Processing'''
+    # Adding rectangle to image 
+    cv2.rectangle(img, (50,50),(400,400),(0,255,0),2)
+    # Capturing image within rectangle and resizing it accordingly 
+    frame_img = cv2.resize(img[50:400,50:400],(IMG_WIDTH,IMG_HEIGHT))
+    
+    # Image conversion to grayscale and smoothing
+    gray = cv2.cvtColor(frame_img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(gray, (3,3), 0)
+
+    # sobel_img = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+    # Applying binary threshold to image
+    ret, binary_img = cv2.threshold(img_blur, 80, 255, cv2.THRESH_BINARY_INV)
+
+    # Implementing shape correction
+    # frame_img = np.expand_dims(frame_img, axis=0)
+    # sobel_img = np.expand_dims(sobel_img, axis=0)
+    binary_img = np.expand_dims(binary_img, axis=0)
+
+
+    # Predicting image from model
+    # prediction = model.predict(frame_img)
+    # prediction = model.predict(sobel_img)
+    prediction = model.predict(binary_img)
+
+    # Acquire correct letter from predicted value from dictionary
+    predicted_label = np.argmax(prediction, axis=1)
+    predicted_letter = list(letters_with_labels.keys())[list(letters_with_labels.values()).index(predicted_label)]
+
+    # Outputting predicted letter to screen    
+    font=cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, predicted_letter, (30,30), font, fontScale=1, color=(255,0,0), thickness=2)
+
+    cv2.imshow('Letter', img)
+
+    # Press 'q' to quit program
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ vowel_static_images_test.py
@@ -0,0 +1,132 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import load_model
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import time
+import mediapipe as mp
+
+vowels = ['A', 'E', 'I', 'O', 'U', 'Y']
+vowels_with_labels = {vowel : i for i, vowel in enumerate(vowels)}
+
+# Defining image sizes
+IMG_WIDTH=200
+IMG_HEIGHT=200
+
+def load_test_data():
+    # Defining correct directory to load images from 
+    dir = r'C:\Users\kunal\Documents\ME369P\Final_Project\vowel_test_images'
+    print('Loading Test Images')
+
+    original_images = []
+    test_images = []
+    test_labels = []
+    # Looping through each image in test images folder
+    for file in os.listdir(dir):
+        # Conditions if only wanting to predict on vowels
+        # if (str(file[0]) in vowels):
+
+        # Assigning numeric label based on letter of file
+        label = vowels_with_labels[str(file[0])]
+
+        # Getting path of each image
+        img_path = os.path.join(dir, file)
+
+        # Acquiring and preprocessing image to plot
+        img = cv2.imread(img_path)
+
+        img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+        orig_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        orig_img = orig_img.astype('float32')
+        orig_img /= 255
+        original_images.append(orig_img)
+        
+        # Converting images to grayscale and smoothing them
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.GaussianBlur(gray, (3,3), 0)
+        
+        # Applying binary threshold to images
+        ret, binary = cv2.threshold(img_blur, 80, 255, cv2.THRESH_BINARY_INV)
+
+        test_images.append(binary)
+        test_labels.append(label)
+        
+
+    # Converting image list to numpy arrrays
+    test_images = np.array(test_images, dtype='float32')
+    original_images = np.array(original_images, dtype='float32')
+    test_labels = np.array(test_labels, dtype='int32')
+
+    return test_images, original_images, test_labels
+
+# Loading images and randomly shuffling them
+test_images, original_images, test_labels = load_test_data()
+test_images, original_images, test_labels = shuffle(test_images, original_images, test_labels, random_state=25)
+
+# Get 28 random images with their labels only from custom test folder
+num_custom_images = 1200
+random_idx = np.random.choice(np.arange(num_custom_images), 28, replace=False)
+random_orig_images = original_images[random_idx]
+random_test_images = test_images[random_idx]
+random_test_labels = test_labels[random_idx]
+
+# next 2 lines only needed if implementing grayscale model
+# numberImages = np.size(test_images, 0)
+# train_images = test_images.reshape(numberImages, IMG_WIDTH, IMG_HEIGHT, 1)
+
+'''Model to Load'''
+model = load_model('vowel_thresholding_model')
+
+# Evaluating model accuracy
+# eval = model.evaluate(test_images, test_labels)
+eval = model.evaluate(random_test_images, random_test_labels)
+print('Evaluation Accuracy: ' + str(eval[1]))
+
+# Predicting labels for each image
+# class_predictions = model.predict(test_images)
+class_predictions = model.predict(random_test_images)
+predicted_label = np.argmax(class_predictions, axis=1)
+
+
+# Matching letter to numeric prediction
+actual_letters = []
+predicted_letters = []
+# for i in range(len(test_labels)):
+#     actual_letter = list(letters_with_labels.keys())[list(letters_with_labels.values()).index(test_labels[i])]
+#     predicted_letter = list(letters_with_labels.keys())[list(letters_with_labels.values()).index(predicted_label[i])]
+
+#     actual_letters.append(actual_letter)
+#     predicted_letters.append(predicted_letter)
+
+for i in range(len(random_test_labels)):
+    actual_letter = list(vowels_with_labels.keys())[list(vowels_with_labels.values()).index(random_test_labels[i])]
+    predicted_letter = list(vowels_with_labels.keys())[list(vowels_with_labels.values()).index(predicted_label[i])]
+
+    actual_letters.append(actual_letter)
+    predicted_letters.append(predicted_letter)
+
+# Printing resulting arrays
+print('Actual Labels:')
+print(actual_letters)
+print('Predicted Labels:')
+print(predicted_letters)
+
+
+# Visualize predictions
+L = 7
+W = 4
+
+fig, axes = plt.subplots(nrows=L, ncols=W, figsize = (15,15))
+axes = axes.ravel()
+
+for i in np.arange(0, 28):  #26
+    # axes[i].imshow(original_images[i])#.reshape(200,200,3))
+    axes[i].imshow(random_orig_images[i])#.reshape(200,200,3))
+    axes[i].set_title(f"Prediction Class = {predicted_letters[i]}\n True Class = {actual_letters[i]}", fontsize=12)
+    axes[i].axis('off')
+plt.subplots_adjust(wspace=0.5, hspace=1)
+plt.show()
